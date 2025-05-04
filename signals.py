9c3614e7cedf695be5a474f6b1daa3fa0ca901d0
Arin-nana <peoplewow02@gmail.com>
2025-05-04 19:05:15 +0500

    Create signals.py

---
+++ signals.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from django.utils.timezone import now
+from django.urls import resolve
+from .models import TelegramSubscriber
+from .telegram_bot import bot
+
+@receiver(user_logged_in)
+def notify_admin_login(sender, request, user, **kwargs):
+    resolver = resolve(request.path_info)
+    if resolver.namespace == 'admin':
+        login_time = now().strftime('%Y-%m-%d %H:%M:%S')
+        text = f"Дата входа: {login_time}\nИмя пользователя: {user.username}"
+        for sub in TelegramSubscriber.objects.all():
+            try:
+                bot.send_message(sub.chat_id, text)
+            except Exception as e:
+                pass
