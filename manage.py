d77e3b6cb9e1ecc78cb293925fa83c6bd7640e74
Arin-nana <peoplewow02@gmail.com>
2025-05-04 19:59:53 +0500

    refactor

--- manage.py
+++ manage.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+"""Django’s command-line utility for administrative tasks."""
 import os
 import sys
 
 def main():
-    # поскольку settings.py лежит рядом с этим файлом,
-    # импортируем просто 'settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+    # Если settings.py лежит в этой же папке:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')    # Если вы впоследствии вынесете settings.py в подпапку myproject,
+    # замените на 'myproject.settings'
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Не удалось импортировать Django. Убедитесь, что оно установлено "
-            "и активна виртуальная среда."
+            "Не удалось импортировать Django. Убедитесь, что виртуальная среда активирована "
+            "и Django установлено."
         ) from exc
     execute_from_command_line(sys.argv)
 
--- urls.py
+++
@@ -1,7 +0,0 @@
-# myapp/urls.py
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # namespace="admin" — по умолчанию
-]
